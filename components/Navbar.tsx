3384c14c3c44445bfd9af105cacd908c387697f0
shresthbhargava <shresthbhargava835@gmail.com>
2025-08-03 19:06:40 +0530

    Fix Navbar syntax errors and clear cache

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Navitems from "@/components/Navitems";
-const Navbar = () ⇒ {
+const Navbar = () => {
     return(
     <nav className="navbar">
         <Link href="/public">
@@ -17,6 +17,8 @@ const Navbar = () ⇒ {
         <div className="flex items-center gap-8">
             <Navitems/>
             <p>Sign In</p>
-        </div></nav>
+        </div>
+    </nav>
     )
+}
 export default Navbar
