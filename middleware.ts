1eeae7f4af9bb6816e122f343e6f146350aaf745
shresthbhargava <shresthbhargava835@gmail.com>
2025-08-03 19:03:31 +0530

    Fix middleware named export

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export default function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   return NextResponse.next()
 }
